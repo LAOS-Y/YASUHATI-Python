1fd4f5ab6ce113d60d86daf7f1ab52e3045c6142
LAOS-Y <billyang426@gmail.com>
2020-04-22 20:38:30 +0800

    add jump

--- main.py
+++ main.py
@@ -7,41 +7,50 @@ class Player(cocos.sprite.Sprite):
     def __init__(self, img_path='player.png', init_pos=(0, 0)):
         super().__init__(img_path)
 
-        self.init_pos = init_pos
-        # self.position = init_pos
-        # self.speed = 0
+        self.position = init_pos
+        self.speed = 0
+        self.landed = True
 
-        self.reset()
+        # self.schedule(self.fall)
 
-        self.schedule(self.update)
+    def jump(self):
+        self.speed = 5
+        self.landed = False
 
-    def jump(self, dt):
-        self.speed = 50
-
-    def update(self, dt):
+    def fall(self, dt):
         self.speed -= 10 * dt
         self.y += self.speed
-        print(self.y)
-        if self.y < 100:
-            self.reset()
+        print(self.y, self.speed)
 
-    def reset(self):
+    def land(self):
+        self.landed = True
         self.speed = 0
-        self.position = self.init_pos
 
 
 class Game(cocos.layer.Layer):
     def __init__(self):
         super().__init__()
         # player = cocos.sprite.Sprite('player.png')
-        player = Player(init_pos=(WIN_WIDTH / 2, WIN_HEIGHT / 2))
+        self.player = Player(init_pos=(WIN_WIDTH / 2, WIN_HEIGHT / 2))
         # player.position = 
 
-        self.add(player)
+        self.add(self.player)
+
+        self.schedule(self.update)
+
+    def update(self, dt):
+        if self.player.landed:
+            self.player.jump()
+
+        self.player.fall(dt)
+
+        if self.player.y <= WIN_HEIGHT / 2:
+            self.player.land()
+
 
 
 director.init(width=WIN_WIDTH, height=WIN_HEIGHT)
 
-hello_layer = Game()
-main_scene = cocos.scene.Scene(hello_layer)
+game_layer = Game()
+main_scene = cocos.scene.Scene(game_layer)
 director.run(main_scene)
